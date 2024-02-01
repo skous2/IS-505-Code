6f025c356287d2fd84fe70e087e690b4d7b09a83
skous2 <tanner.skousen@gmail.com>
2024-02-01 15:10:21 -0700

    Added Day 11

---
+++ greetings.py
@@ -0,0 +1,14 @@
+def english(name):
+    return f'Hi {name}!'
+
+def mandarin(name):
+    return f'Ni Hao {name}!'
+
+def marshallese(name):
+    return f'Iakwe {name}!'
+
+def portuguese(name):
+    return f'Oi {name}!'
+
+def japanese(name):
+    return f'Konnichiwa {name}!'
